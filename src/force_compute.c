30595ad65c1119cef7d54a0a71ede65338e1343e
Zainab Nazari <nazari.zainab@gmail.com>
2022-03-03 09:32:57 +0100

    math easing technics

--- src/force_compute.c
+++ src/force_compute.c
@@ -4,10 +4,20 @@
 /* compute forces */
 void force(mdsys_t *sys)
 {
-    double r,ffac;
+    double ffac;
     double rx,ry,rz;
-    double epot = 0.0;
+    double epot ;
     int i,j;
+// this change of paprameters done for avoiding expensive math like power, sqrt, division.
+     double sigma1=sys->sigma;
+     double sigma6=(sigma1)*(sigma1)*(sigma1)*(sigma1)*(sigma1)*(sigma1);
+     double sigma12=sigma6*sigma6;
+    double rsq,rinv,r6;
+    rinv=1.0/rsq;
+    r6=rinv*rinv*rinv;
+     double c12 = 4.0 * sys->epsilon * sigma6 * sigma6 ;
+     double c6  = 4.0 * sys->epsilon * sigma6 ;
+     double rcsq = sys->rcut * sys->rcut;
 
     /* zero energy and forces */
     azzero(sys->fx,sys->natoms);
@@ -24,38 +34,36 @@ void force(mdsys_t *sys)
             rx=pbc(sys->rx[i] - sys->rx[j], 0.5*sys->box);
             ry=pbc(sys->ry[i] - sys->ry[j], 0.5*sys->box);
             rz=pbc(sys->rz[i] - sys->rz[j], 0.5*sys->box);
-            r = sqrt(rx*rx + ry*ry + rz*rz);
+            rsq =rx*rx + ry*ry + rz*rz;
+
 
             /* compute force and energy if within cutoff */
-            if (r < sys->rcut) {
-                ffac = -4.0*sys->epsilon*(-12.0*pow(sys->sigma/r,12.0)/r
-                                         +6*pow(sys->sigma/r,6.0)/r);
-                
+            if (rsq < rcsq) {
+                ffac = (12.0*c12*r6 - 6.0*c6)*r6*rinv;
 
-                epot += 4.0*sys->epsilon*(pow(sys->sigma/r,12.0)
-                                               -pow(sys->sigma/r,6.0));
+                sys->epot += r6*(c12*r6-c6);
 
-            
+
                 #ifdef _OPENMP
                 #pragma omp atomic
-                sys->fx[i] += rx / r * ffac;
+                sys->fx[i] += rx * ffac;
                 #pragma omp atomic
-                sys->fy[i] += ry / r * ffac;
+                sys->fy[i] += ry  * ffac;
                 #pragma omp atomic
-                sys->fz[i] += rz / r * ffac;
+                sys->fz[i] += rz  * ffac;
                 #pragma omp atomic
-                sys->fx[j] -= rx / r * ffac;
+                sys->fx[j] -= rx  * ffac;
                 #pragma omp atomic
-                sys->fy[j] -= ry / r * ffac;
+                sys->fy[j] -= ry * ffac;
                 #pragma omp atomic
-                sys->fz[j] -= rz / r * ffac;
+                sys->fz[j] -= rz * ffac;
                 #else
-                sys->fx[i] += rx / r * ffac;
-                sys->fy[i] += ry / r * ffac;
-                sys->fz[i] += rz / r * ffac;
-                sys->fx[j] -= rx / r * ffac;
-                sys->fy[j] -= ry / r * ffac;
-                sys->fz[j] -= rz / r * ffac;
+                sys->fx[i] += rx  * ffac;
+                sys->fy[i] += ry * ffac;
+                sys->fz[i] += rz  * ffac;
+                sys->fx[j] -= rx * ffac;
+                sys->fy[j] -= ry  * ffac;
+                sys->fz[j] -= rz * ffac;
                 #endif
             }
         }
